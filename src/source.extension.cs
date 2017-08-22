2031aa5452aabe70857c0221b72f8b835446c52a
Nikhil <nkhurana50@gmail.com>
2017-08-22 09:55:37 +0530

    Initial Commet.

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,11 +8,11 @@ namespace XHelper
     static class Vsix
     {
         public const string Id = "d7c3f904-cc5a-4d47-aa25-81fb7c36df89";
-        public const string Name = "Comment Remover";
-        public const string Description = @"Remove all comments in any file with a click of a button. Can also remove #regions and preserve XML Doc comments.";
+        public const string Name = "XML Helper";
+        public const string Description = @"Provides clean way to decode <,>,&<,&gt; >";
         public const string Language = "en-US";
         public const string Version = "1.3";
-        public const string Author = "Mads Kristensen";
+        public const string Author = "Nikhil Khurana";
         public const string Tags = "comment, cleanup, delete, region, XMLDoc";
     }
 }
